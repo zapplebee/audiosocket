b91f4a3361d0bd0ce76a6913adf2557577fb0994
Zachary Skalko <zskalko@gmail.com>
2016-12-19 22:47:18 -0600

    Major update

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
+"use strict"
+
 var express = require('express');
 var app = express();
 var sassMiddleware = require('node-sass-middleware');
 var path = require('path');
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+const _ = require('lodash');
+
+
+const toBuffer = require('typedarray-to-buffer')
+const soxPath = require('sox-bin');
+const cp = require('child_process');
+const stream = require('stream');
+
+const ARGS = [
+  '-r',
+  44100,
+  '-e',
+  'floating-point',
+  '-b',
+  '32',
+  '-c',
+  '1',
+  '-t',
+  'raw',
+  '-',
+  '-t',
+  'flac',
+  '-'
+];
+
+
+const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+
+
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+server.listen(80);
+
+
 
 //pug renderer, index
 app.set('views', './views')
@@ -12,7 +51,8 @@ app.get('/', function (req, res) {
 
 
 app.use('/script/angular.js', express.static(__dirname + '/node_modules/angular/angular.js'));
-app.use('/script', express.static(__dirname + '/src'));
+app.use('/script/socket.io.min.js', express.static(__dirname + '/node_modules/socket.io-client/dist/socket.io.min.js'));
+app.use('/script', express.static(__dirname + '/script'));
 
 
 //sass
@@ -27,9 +67,28 @@ app.use("/style", express.static(path.join(__dirname, '/style')));
 
 
 app.use("/images", express.static(path.join(__dirname, 'images')));
-app.set('port', (process.env.PORT || 5000));
 
-//listen
-app.listen(app.get('port'), function () {
-  console.log('localhost:'+app.get('port'));
+  
+io.on('connection', function (socket) {
+
+  var speech_to_text = new SpeechToTextV1({
+    "password": "",
+    "username": ""
+      });
+  
+  const sox = spawn(soxPath, ARGS);
+  var bufferStream = new stream.PassThrough();
+  bufferStream.pipe(sox.stdio[0]);  
+  sox.stdio[1].pipe(speech_to_text.createRecognizeStream({ content_type: 'audio/flac; rate=44100' }))
+  .on('error',function(e){
+    console.log(e);
+    socket.emit('error',e);
+  })
+  .on('data',function(b){
+    socket.emit('transcript',b.toString());
+  })
+  
+  socket.on('audio', function (buffer) {
+    bufferStream.write(buffer);
+  });
 });
--- script/main.js
+++ script/main.js
@@ -6,3 +6,34 @@ app.controller('navctrl', function($scope,$timeout){
    
   
 })
+
+var socket = io.connect('http://localhost');
+socket.on('data', function (data) {
+  console.log(data);
+});
+
+
+var sampleRate = 44100;
+var inputBufferLength =  8192;
+function initializeRecorder(stream) {
+  var context = new AudioContext();
+  var audioInput = context.createMediaStreamSource(stream);
+  var listener = context.createScriptProcessor(inputBufferLength, 1, 1);
+  var silenceNotes = 0;
+  var listnening = false;
+  var lastListen = 0;
+  listener.onaudioprocess = function(e) {
+    var leftChannel = e.inputBuffer.getChannelData(0);
+    var emitInterval = Math.abs(lastListen - e.timeStamp);
+    sampleRate = e.inputBuffer.sampleRate;
+    socket.emit('audio',leftChannel.buffer);
+  }
+
+  audioInput.connect(listener);
+  listener.connect(context.destination);
+}
+
+socket.on('transcript',function(e){console.log(e)});
+socket.on('error',function(e){console.error(e)});
+
+navigator.getUserMedia({audio: true,video: false}, initializeRecorder, function(e){console.log(e);});
